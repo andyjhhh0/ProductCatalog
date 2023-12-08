8ba2130be0446d0df72201572efc1c71f7bcd6c9
Andy How Hok Hium <ahowh089@uottawa.ca>
2023-12-07 22:20:31 -0500

    Probably a fix to checkprice, hopefully the last

--- app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
+++ app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
@@ -20,7 +20,7 @@ public class ProductTest {
     public void checkProductPrice() {
         // I am making this test to fail
         Product aProduct = new Product("1", "DELL MONITOR", 180);
-        String price= Double.tostring(aProduct.getPrice());
+        String price= Double.toString(aProduct.getPrice());
         assertEquals("Check the price of the product", "180.0", price);
     }
 
